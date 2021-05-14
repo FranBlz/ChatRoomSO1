d8a8551ebadd73ddd1a4dfed9c5140013978c7f8
Francisco Bolzan <franblz99@gmail.com>
2021-05-14 10:25:33 -0300

    IPC funcionando, problema sobrecarga asistente

--- RemoteMultiThreadServer.c
+++ RemoteMultiThreadServer.c
@@ -1,15 +1,9 @@
-/* RemoteMultiThreadServer.c */
-/* Cabeceras de Sockets */
 #include <sys/types.h>
 #include <sys/socket.h>
-/* Cabecera de direcciones por red */
 #include <netinet/in.h>
-/**********/
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-/**********/
-/* Threads! */
 #include <pthread.h>
 #include <string.h>
 
@@ -27,14 +21,14 @@ void *workers(void *arg);
 void error(char *msg);
 
 int main(int argc, char **argv){
+  if (argc <= 1) error("Faltan argumentos");
+
   int sock, *soclient;
   struct sockaddr_in servidor, clientedir;
   socklen_t clientelen;
   pthread_t thread;
   pthread_attr_t attr;
   int *socketsAssistant = malloc(sizeof(int)*2);
-
-  if (argc <= 1) error("Faltan argumentos");
 
   /* Creamos el socket */
   if( (sock = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
@@ -48,15 +42,12 @@ int main(int argc, char **argv){
   /* Inicializamos el socket */
   if (bind(sock, (struct sockaddr *) &servidor, sizeof(servidor)))
     error("Error en el bind");
-
   printf("Binding successful, and listening on %s\n",argv[1]);
 
-  /************************************************************/
   /* Creamos los atributos para los hilos.*/
-  pthread_attr_init(&attr);
   /* Hilos que no van a ser *joinables* */
+  pthread_attr_init(&attr);
   pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
-  /************************************************************/
 
   /* Ya podemos aceptar conexiones */
   if(listen(sock, MAX_CLIENTS) == -1)
@@ -68,7 +59,7 @@ int main(int argc, char **argv){
     exit(1);
   }
 
-  pthread_create(&thread , NULL , assistant, (void *) socketsAssistant);
+  pthread_create(&thread , NULL , assistant, (void *) &socketsAssistant[0]);
 
   for(;;){ /* Comenzamos con el bucle infinito*/
     /* Pedimos memoria para el socket */
@@ -83,13 +74,16 @@ int main(int argc, char **argv){
     }
 
     /* Le enviamos el socket al hijo*/
-    int *sockets[] = {soclient, socketsAssistant};
+    int *sockets[] = {soclient, &socketsAssistant[1]};
     pthread_create(&thread , NULL , workers, (void *) sockets);
 
     /* El servidor puede hacer alguna tarea más o simplemente volver a esperar*/
   }
 
   /* Código muerto */
+  free(soclient);
+  close(socketsAssistant[1]);
+  close(socketsAssistant[0]);
   close(sock);
   return 0;
 }
@@ -100,57 +94,58 @@ int main(int argc, char **argv){
  * - envía el socket de un nickname al hilo que lo pida
  */
 void *assistant(void *_arg) {
-  int *sockets = (int*) _arg;
-  char buf[MAX_LENGTH];
   // Assistant usa canal 0
-  close(sockets[0]);
-  /* Leemos lo que venga por el socket */
+  int socketWorkers = *(int*) _arg;
+  char buf[MAX_LENGTH];
+  char nicknames[MAX_CLIENTS][30];
+  int addresses[MAX_CLIENTS];
+
   while(1) {
-    if((read(sockets[1], buf, sizeof(buf))) < 0)
+    if((read(socketWorkers, buf, sizeof(buf))) < 0)
       perror("Parent error while reading"), exit(1);
 
     printf("Asistente recibió: %s\n", buf);
 
     /*Escribimos nuestro mensaje */
-    if((write(sockets[1], "ACK", sizeof("ACK"))) < 0)
+    if((write(socketWorkers, "ACK", sizeof("ACK"))) < 0)
       perror("Parent error while writing"),exit(1);
   }
-
-  close(sockets[1]);
 }
 
 void * workers(void *_arg){
   int **sockets = (int**)_arg;
   int socket = *sockets[0];
-  int *assistantSock = sockets[1];
-  close(assistantSock[1]);
-  char buf[MAX_LENGTH], nick[MAX_LENGTH];
+  int socketAssistant = *sockets[1];
+  char buf[MAX_LENGTH], nick[30];
 
   send(socket, "Ingrese su nickname: ", sizeof("Ingrese su nickname: "), 0);
   recv(socket, nick, sizeof(nick), 0);
+
+  /* nick + socket */
+  if((write(socketAssistant, buf, sizeof(buf))) < 0)
+    perror("Parent error while writing"),exit(1);
+
+
 
   int i = 1;
   while(i) {
     recv(socket, buf, sizeof(buf), 0);
     printf("%s: %s\n", nick, buf);
 
-    if(strcmp(buf,"/msg")) { //quiero probar algo, dame un touch
+    if(!strcmp(buf,"/msg")) {
       /*Escribimos nuestro mensaje */
-      if((write(assistantSock[0], buf, sizeof(buf))) < 0)
+      if((write(socketAssistant, buf, sizeof(buf))) < 0)
         perror("Parent error while writing"),exit(1);
 
       /* Leemos lo que venga por el socket */
-      if((read(assistantSock[0], buf, sizeof(buf))) < 0)
+      if((read(socketAssistant, buf, sizeof(buf))) < 0)
         perror("Parent error while reading"), exit(1);
       printf("Worker recibió: %s\n", buf);
-
     }
 
     i = strcmp(buf,"/exit");
   }
-  close(assistantSock[0]);
   printf("%s ha salido\n", nick);
-  free((int*)_arg);
   return NULL;
 }
 
